5c3eae5cee19efc8b7eef6ce1f2940a76ec84071
NeBugAFicha <dawn.bilan@yandex.ru>
2020-10-16 14:37:44 +0300

    release v4

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -12,7 +12,7 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
-
+    private String filename;
 
 
     public Message() {
@@ -54,5 +54,11 @@ public class Message {
         this.tag = tag;
     }
 
+    public String getFilename() {
+        return filename;
+    }
 
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 }
